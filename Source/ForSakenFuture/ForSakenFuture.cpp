1178d4a75739fde0797098467539c00609b4ffaf
Thunder <thunderpwn@users.noreply.github.com>
2020-11-11 14:05:40 +0100

    Clean it all

--- Source/ForSakenFuture/ForSakenFuture.cpp
+++
@@ -1,6 +0,0 @@
-// (C) StoryTimeStudio 2020
-
-#include "ForSakenFuture.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ForSakenFuture, "ForSakenFuture" );
